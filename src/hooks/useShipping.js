e219a1d0e85513b79d8fc7bb4e3debba8de84761
Megabyte-webdev <afolabimubarak18@gmail.com>
2025-11-02 01:14:22 +0100

    vendor setup flow- shipping completed

---
+++ src/hooks/useShipping.js
@@ -0,0 +1,85 @@
+import { useContext } from "react";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { axiosClient } from "../services/axios-client";
+import { AuthContext } from "../context/AuthContext";
+import { onFailure } from "../utils/notifications/OnFailure";
+import { onSuccess } from "../utils/notifications/OnSuccess";
+import { extractErrorMessage } from "../utils/formatters";
+
+const useShipping = () => {
+  const { authDetails } = useContext(AuthContext);
+  const client = axiosClient(authDetails?.access_token);
+  const queryClient = useQueryClient();
+
+  const handleFailure = (action, error) => {
+    const message = extractErrorMessage(error);
+    onFailure({ title: `${action} Failed`, message });
+  };
+
+  const handleSuccess = (action, message) => {
+    onSuccess({ title: `${action} Successful`, message });
+  };
+
+  // SHIPMENT INFORMATION
+
+  // Get Shipment by User ID
+  const useGetShipmentByUser = (userId) =>
+    useQuery({
+      queryKey: ["shipment", userId],
+      queryFn: async () => {
+        const response = await client.get(`/store/shipment/${userId}`);
+        return response.data.data;
+      },
+      enabled: !!userId && !!authDetails?.access_token,
+      onError: (error) => handleFailure("Fetch Shipment Info", error),
+    });
+
+  // Create Shipment Info
+  const createShipment = useMutation({
+    mutationFn: async (shipmentData) => {
+      const response = await client.post("/store/shipment/", shipmentData);
+      return response.data.data;
+    },
+    onSuccess: () => {
+      handleSuccess("Shipment Creation", "Shipment info created successfully!");
+      queryClient.invalidateQueries(["shipment"]);
+    },
+    onError: (error) => handleFailure("Shipment Creation", error),
+  });
+
+  // Update Shipment Info
+  const updateShipment = useMutation({
+    mutationFn: async (shipmentData) => {
+      const response = await client.put("/store/shipment/", shipmentData);
+      return response.data.data;
+    },
+    onSuccess: () => {
+      handleSuccess("Shipment Update", "Shipment info updated successfully!");
+      queryClient.invalidateQueries(["shipment"]);
+    },
+    onError: (error) => handleFailure("Shipment Update", error),
+  });
+
+  // Delete Shipment Info
+  const deleteShipment = useMutation({
+    mutationFn: async () => {
+      const response = await client.delete("/store/shipment/");
+      return response.data.data;
+    },
+    onSuccess: () => {
+      handleSuccess("Shipment Deletion", "Shipment info deleted successfully!");
+      queryClient.invalidateQueries(["shipment"]);
+    },
+    onError: (error) => handleFailure("Shipment Deletion", error),
+  });
+
+  return {
+    // Shipment
+    useGetShipmentByUser,
+    createShipment,
+    updateShipment,
+    deleteShipment,
+  };
+};
+
+export default useShipping;
