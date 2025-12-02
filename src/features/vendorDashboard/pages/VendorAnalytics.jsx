af30bc5416b3b8af2585690b1848283115e8b5a5
Abdulazeez782 <oludareabdazeez2002@gmail.com>
2025-12-02 19:51:10 +0100

    worked on the vendor customers and analytics page

--- src/features/vendorDashboard/pages/VendorAnalytics.jsx
+++ src/features/vendorDashboard/pages/VendorAnalytics.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import SharedMetricsCard from "../components/SharedMetricsCard"
+import { IoBarChartOutline } from "react-icons/io5";
 
 const VendorAnalytics = () => {
   return (
-    <div>VendorAnalytics</div>
+    <main>
+      <section>
+        <section>
+
+        </section>
+
+        <section>
+          
+        </section>
+      </section>
+
+      <SharedMetricsCard 
+        header={"Advanced Analytics"}
+        paragraphText={"Detailed chartss and analytics will appear here as your store generates more data"}
+        icon={IoBarChartOutline}
+        buttonLabel={"Set Up Analytics Tracking"}
+      />
+    </main>
   )
 }
 
--- src/features/vendorDashboard/pages/VendorCustomers.jsx
+++ src/features/vendorDashboard/pages/VendorCustomers.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { GoPeople } from "react-icons/go";
+import SharedMetricsCard from "../components/SharedMetricsCard";
 
 const VendorCustomers = () => {
   return (
-    <div>VendorCustomers</div>
+    <main>
+      <SharedMetricsCard 
+        icon={GoPeople}
+        header={"Customer Management"}
+        subtext={"View and manage your customer relationships"}
+        paragraphText={"Advanced customer management tools will be available here once you have more customer data"}
+        buttonLabel={"View Customer Insights"}
+      />
+    </main>
+    
   )
 }
 
