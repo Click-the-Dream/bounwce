659f6b1b28844366df0727e337442ce2bd8691dd
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2026-03-07 04:05:06 +0100

    Update Header.jsx

--- src/pages/Landing/sections/Header.jsx
+++ src/pages/Landing/sections/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
     <div className="flex justify-between items-center gap-2 fixed w-full z-50 bg-white dark:bg-gray-900 border-b border-b-gray-300 dark:border-gray-800 px-4 py-7 lg:py-6 transition-colors duration-300">
         
         <a href="/" className="text-md md:text-xl lg:text-[1.75rem] font-sthupo font-black tracking-[0.15em] text-[#1A1A1A] dark:text-white transition-colors duration-300">
-            BOU<span className="text-brand-orange">W</span>NCE
+            bou<span className="text-brand-orange">w</span>nce
         </a>
 
         {/* Wrap buttons in a flex container */}
