933d9368dc487e29274b3c957bf50797dde48fbc
Megabyte-webdev <afolabimubarak18@gmail.com>
2026-04-04 18:57:36 +0100

    font fix

--- src/pages/Landing/context/ThemeContext.jsx
+++ src/pages/Landing/context/ThemeContext.jsx
@@ -7,7 +7,7 @@ export const ThemeProvider = ({ children }) => {
     if (typeof window !== 'undefined') {
       const savedTheme = localStorage.getItem('theme');
       if (savedTheme) return savedTheme;
-      //if (window.matchMedia('(prefers-color-scheme: dark)').matches) return 'dark';
+      if (window.matchMedia('(prefers-color-scheme: dark)').matches) return 'dark';
     }
     return 'light';
   });
