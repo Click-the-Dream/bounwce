7862da3cabd0c9c7fd08ec5c3702da9dddceb557
Megabyte-webdev <afolabimubarak18@gmail.com>
2026-04-05 22:41:27 +0100

    latest

--- src/pages/Landing/components/CategoryList.jsx
+++ src/pages/Landing/components/CategoryList.jsx
@@ -1,14 +1,28 @@
 import { useNavigate } from 'react-router-dom'; // Added
 import useProduct from '../../../hooks/useProduct';
+import { onPrompt } from '../../../utils/notifications/onPrompt';
 
+const categories = [
+    { name: 'Concert buddy' },
+    { name: 'Church community' },
+    { name: 'Weekend date' },
+    { name: 'Football partners' },
+    { name: 'Jollof rice' },
+    { name: 'Startup Events' },
+    { name: 'Shawarma near me' },
+]
 const CategoryList = () => {
     const navigate = useNavigate();
     const { useGetProductCategories } = useProduct();
-    const { data: categories = [] } = useGetProductCategories();
+    //const { data: categories = [] } = useGetProductCategories();
 
     const handleCategoryClick = (categoryName) => {
+        onPrompt({
+            title: "Category search coming soon!",
+            message: "We're working hard to bring you this feature. Stay tuned for updates!",
+        })
         // Navigates to /products?category=CategoryName
-        navigate(`/marketplace?category=${encodeURIComponent(categoryName)}`);
+        // navigate(`/marketplace?category=${encodeURIComponent(categoryName)}`);
     };
 
     return (
@@ -17,7 +31,7 @@ const CategoryList = () => {
                 <button
                     key={index}
                     onClick={() => handleCategoryClick(cat?.name)}
-                    className="cursor-pointer px-5 py-2 text-[12px] font-medium border border-gray-300 rounded-full bg-white/50 hover:bg-white transition-all text-gray-700 active:scale-95"
+                    className="cursor-pointer px-5 py-2 text-[12px] font-medium border-[0.5px] border-[#BDBDBD] rounded-full bg-white hover:bg-white/50 transition-all text-gray-700 active:scale-95"
                 >
                     {cat?.name}
                 </button>
